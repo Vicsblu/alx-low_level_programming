7eaa8efded560628cb56408e6085ef3522bfe15f
Vicsblu <oguntoyinbo50@gmail.com>
2022-09-14 19:01:46 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include "main.h"
 /**
-* main - "Code entry"
+* main - Code entry
 *
-* Description: This program prints _putchar
+* Description: This program prints _putchar
 *
-* Return: 0
+* Return: 0
 */
-int main(void)
+int main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -17,5 +17,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-'return' (0);
+return (0);
 }
