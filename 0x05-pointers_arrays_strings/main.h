00b976532dea3fe17e82422d42e381ffeed73177
Vicsblu <oguntoyinbo50@gmail.com>
2022-09-19 17:09:23 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
