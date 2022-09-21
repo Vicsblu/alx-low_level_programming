2e0481be030507962c362c73e8ac9fda6e11098f
Vicsblu <oguntoyinbo50@gmail.com>
2022-09-21 11:50:38 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,14 @@ void char *_strcat(char *dest, char *src)
 	int length,  j;
 
 	length  =  0;
-	while (dest[length] '        !=' ' \0 ')
+	while (dest[length] '!=' ' \0 ')
 {
 	length++;
 }
 for (j = 0;  src[j] !=  ' \0 ';  j++,  length++)
 {
-	dest[length]  '='  src[j];
+	dest[length]'='src[j];
 }
-dest[length]  '='  ' \0 '
+dest[length]'=' '\0 '
 return (dest)
 }
